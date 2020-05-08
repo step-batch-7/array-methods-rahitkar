74b3cc4999731b3ea9c97f11490120b8a09ddc40
Rahit <rahitkar@users.noreply.github.com>
2020-05-09 01:15:04 +0530

    added test for testing filter

--- test/test_array_methods.c
+++ test/test_array_methods.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "../array.h"
 #include "compare_array.h"
+
 
 int sqr(int num)
 {
@@ -10,9 +12,25 @@ int sqr(int num)
 
 void test_map(Array_ptr array)
 {
+  printf("testing map:\n");
   int expectation[4] = {1, 4, 9, 16};
   Array_ptr new_array = map(array, &sqr);
   compare_arrayes(new_array->array, expectation, 4);
+  printf("\n");
+}
+
+Bool is_even(int num)
+{
+  return !(num % 2);
+}
+
+void test_filter(Array_ptr array)
+{
+  printf("testing filter:\n");
+  int expectation[4] = {2, 4};
+  Array_ptr new_array = filter(array, &is_even);
+  compare_arrayes(new_array->array, expectation, 2);
+  printf("\n");
 }
 
 int main(void)
@@ -23,4 +41,5 @@ int main(void)
   array->length = 4;
 
   test_map(array);
+  test_filter(array);
 }
