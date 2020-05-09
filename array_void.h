4f271aacf0d253901d8a83441c79a972ab69b888
Rahit <rahitkar@users.noreply.github.com>
2020-05-09 14:59:11 +0530

    added array_void.h

---
+++ array_void.h
@@ -0,0 +1,25 @@
+#ifndef __ARRAY_VOID_H
+#define __ARRAY_VOID_H
+
+#include "array.h"
+
+typedef void* Void_ptr;
+
+typedef Void_ptr Object;
+typedef Object (*MapperVoid)(Object);
+typedef Bool (*PredicateVoid)(Object);
+typedef Object (*ReducerVoid)(Object, Object);
+
+typedef struct
+{
+  Object *array;
+  int length;
+} ArrayVoid;
+
+typedef ArrayVoid *ArrayVoid_ptr;
+
+ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper);
+ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate);
+Object reduce_void(ArrayVoid_ptr src, Object init, ReducerVoid reducer);
+
+#endif
