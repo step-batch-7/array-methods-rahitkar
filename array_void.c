ee8ff1420c20f8032e10b57fa5bee1d34b8c7750
Rahit <rahitkar@users.noreply.github.com>
2020-05-09 15:19:23 +0530

    added filter_void in array_void.c

--- array_void.c
+++ array_void.c
@@ -11,7 +11,23 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
   for (int indx = 0; indx < src->length; indx++)
   {
     new_array->array[indx] = (*mapper)(src->array[indx]);
-    // printf("%d ", *(int*)new_array->array[indx]);
+  }
+  return new_array;
+}
+
+ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate)
+{
+  ArrayVoid_ptr new_array = malloc(sizeof(ArrayVoid));
+  new_array->array = malloc(src->length * sizeof(Void_ptr));
+  new_array->length = 0;
+
+  for (int indx = 0; indx < src->length; indx++)
+  {
+    if((*predicate)(src->array[indx]) == 1)
+    {
+      new_array->array[new_array->length] = src->array[indx];
+      new_array->length++; 
+    }
   }
   return new_array;
 }
